6620a8849734200d51e7a422279584908150c26a
Daze <44749931+Errleng@users.noreply.github.com>
2022-02-05 16:06:40 -0500

    Add script to deploy commands

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -2,25 +2,27 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const token = process.env.BOT_TOKEN;
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.TEST_SERVER_ID;
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with pong!'),
-  new SlashCommandBuilder()
-    .setName('server')
-    .setDescription('Replies with server info!'),
-  new SlashCommandBuilder()
-    .setName('user')
-    .setDescription('Replies with user info!'),
+    new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with pong!'),
+    new SlashCommandBuilder()
+        .setName('server')
+        .setDescription('Replies with server info!'),
+    new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Replies with user info!'),
 ].map((command) => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest
-  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);

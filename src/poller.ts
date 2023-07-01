646def40ad89756a8213c165066b0106db8ec27d
Eric Liang <44749931+Errleng@users.noreply.github.com>
2023-07-01 19:19:27 -0400

    Use placeholder names instead of real names

--- src/poller.ts
+++ src/poller.ts
@@ -1,5 +1,5 @@
 import { client } from './index';
-import { env, MAX_STATUS_CHARS } from './constants';
+import { env, MAX_STATUS_CHARS, NAME_POOL, STATUS_MESSAGES } from './constants';
 import { ActivityType, GuildMember } from 'discord.js';
 import { splitIntoPhrases } from './utils';
 
@@ -83,42 +83,15 @@ const showActivityMessage = (phrases: string[], curIdx: number, delayBetween: nu
 
 let curMsgIdx = -1;
 const changeActivityMessage = async () => {
-    const guilds = await client.guilds.fetch();
-    const randomGuild = guilds.random();
-    const guild = await randomGuild?.fetch();
-    const users = await guild?.members.fetch();
-    const user = users?.filter((x) => x?.user.bot !== true)?.random();
-
-    const name = `${user?.displayName ?? 'NAME'}`;
-    const messages = [
-        'All-Around Helper is well versed in all manner of cleaning. It was designed to take care of a family household all on its own.',
-        'When it was discovered that All-Around Helper contained a critical error, it had already been sent to a household.',
-        'Blood covered the whole floor, screams echoed, people were running away, and All-Around Helper was gleefully learning the concept of “cleaning”.',
-        'A pristine white surface sleek with fine lines and short but efficient legs, All-Around Helper was created to help people.',
-        'XX Incorporated, the manufacturer of All-Around Helper, was famous for its robotics products.',
-        'All-Around Helper has various functions installed in its body. From burglar alarms and a monitoring system to a coffee maker and light control, it contains everything a household needs.',
-        'You may be surprised to learn the numerous functionalities All-Around Helper contains in its compact body. If you were to dismantle it, you would be even more impressed by how efficiently they packed all the necessary equipment into it.',
-        'If All-Around Helper were to have any capacity of emotion, it would likely feel very proud of itself for all the help it provides to others.',
-        'All-Around Helper always ponders on how it can do more to help.',
-        'Most people are unaware of this, but All-Around Helper makes amazing coffee. Of course, no one is likely to ask it to do such a thing now.',
-        'The helper-bot can help you with any household chores!',
-        'What kind of task do you want me to do? I can help with anything.',
-        'Contamination scan complete. Initiating cleaning protocol.',
-        'Beebeebeep~ Beep. Contamination spotted! Proceeding with trash disposal protocol.',
-        'I will help you with my wide selection of tools.',
-        `All-Around Helper spins around ${name}, however, ${name} continues to concentrate on the task at hand.`,
-        `${name} has decided against buying a robot vacuum cleaner after seeing All-Around Helper.`,
-        `${name} only hopes that All-Around Helper’s “cleaning” mode is never activated.`
-    ];
-
     // max limit is 128 characters
     let newMsgIdx = -1;
     do {
-        newMsgIdx = Math.floor(Math.random() * messages.length);
-    } while (messages.length > 1 && newMsgIdx === curMsgIdx);
+        newMsgIdx = Math.floor(Math.random() * STATUS_MESSAGES.length);
+    } while (STATUS_MESSAGES.length > 1 && newMsgIdx === curMsgIdx);
     curMsgIdx = newMsgIdx;
 
-    const message = messages[curMsgIdx];
+    const name = NAME_POOL[Math.floor(Math.random() * NAME_POOL.length)];
+    const message = STATUS_MESSAGES[curMsgIdx].replace('<name>', name);
     const phrases = splitIntoPhrases(message, MAX_STATUS_CHARS);
     console.debug(`update activity ${curMsgIdx}, ${message.length}, ${phrases.length}`);
 
